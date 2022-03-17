e558b5b5fe790846c5f939d9405d50180b9e0ccb
Harabore <53261144+Harabore@users.noreply.github.com>
2022-03-17 11:57:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port =process.env.PORT || 3000;
-const bodyParser= require('body-Parser');
+const bodyParser= require('body-parser');
 const { response } = require('express');
 const urlEncodedParser =bodyParser.urlencoded({extended:false})
 
